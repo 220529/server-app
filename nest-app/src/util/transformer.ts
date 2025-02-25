e12f8d05534f7e782709a35f71bae8e96f183eba
liukaixin <15713857919@163.com>
2025-02-25 14:41:03 +0800

    test: Exclude

--- nest-app/src/util/transformer.ts
+++ nest-app/src/util/transformer.ts
@@ -3,4 +3,5 @@ import { plainToClass } from "class-transformer";
 // 通用转换函数，用于将普通对象转换为 DTO 类的实例
 export function toDto<T>(entity: any, dtoClass: { new (): T }): T {
   return plainToClass(dtoClass, entity);
+  //   return plainToClass(dtoClass, entity, { excludeExtraneousValues: true });
 }
